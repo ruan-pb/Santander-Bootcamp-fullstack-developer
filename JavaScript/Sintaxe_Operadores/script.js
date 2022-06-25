a757a8cfa0ab58133bee575f7c21571291666b75
ruan-pb <ruancruzsoares@hotmail.com>
2022-06-25 10:19:47 -0300

    Operadores e Sintaxe

---
+++ JavaScript/Sintaxe_Operadores/script.js
@@ -0,0 +1,35 @@
+function verificarValores (a,b){
+    const soma = a+b
+    if(a === b){
+        if(soma > 10 && soma === 20){
+            return `Os numeros ${a} e ${b} são iguais. sua soma é ${soma}, que é maior que 10 e igual a 20`
+        }
+        if(soma === 10 && soma <= 20){
+            return `Os numeros ${a} e ${b} são iguais. sua soma é ${soma}, que é igual a 10 e iguamenor que 20`
+        }
+        if(soma > 10 && soma < 20){
+            return `Os numeros ${a} e ${b} são iguais. sua soma é ${soma}, que é maior que 10 e menor que 20`
+        }
+        if(soma < 10 && soma < 20){
+            return `Os numeros ${a} e ${b} são iguais. sua soma é ${soma}, que é menor que 10 e menor que 20`
+         }
+         if(soma > 10 && soma > 20){
+            return `Os numeros ${a} e ${b} são iguais. sua soma é ${soma}, que é maior que 10 e maior que 20`
+         }
+
+}
+    else{
+        if(soma > 10 && soma < 20){
+            return `Os numeros ${a} e ${b} Não são iguais. sua soma é ${soma}, que é maior que 10 e menor que 20`
+        }
+        if(soma < 10 && soma < 20){
+            return `Os numeros ${a} e ${b} Não são iguais. sua soma é ${soma}, que é menor que 10 e menor que 20`
+         }
+         if(soma > 10 && soma > 20){
+            return `Os numeros ${a} e ${b} Não são iguais. sua soma é ${soma}, que é maior que 10 e maior que 20`
+         }
+
+    }
+}
+
+console.log(verificarValores(1,2));

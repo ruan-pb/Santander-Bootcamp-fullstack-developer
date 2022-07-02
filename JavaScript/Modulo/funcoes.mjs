cb174086afbda61571c44c99f074ad232ec433ff
ruan-pb <ruancruzsoares@hotmail.com>
2022-07-02 12:13:06 -0300

    Orientação a objetos, Dom e Modulo

---
+++ JavaScript/Modulo/funcoes.mjs
@@ -0,0 +1,12 @@
+
+export function mostraIdade(nome, idade) {
+	return `A idade de ${nome} é ${idade}.`;
+}
+
+export function mostraCidade(nome, cidade) {
+	return `A Cidade de ${nome} é ${cidade}.`;
+}
+
+export function mostraHobby(nome, hobby) {
+	return `O Hobby de ${nome} é ${hobby}!`;
+}
---
+++ JavaScript/Modulo/main.js
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Ruan Cruz', 23));
+
+console.log(mostraCidade('Ana Carolina', 'Itapororoca'));
+
+console.log(mostraHobby('Ruan', 'Jogar Futebol'));
